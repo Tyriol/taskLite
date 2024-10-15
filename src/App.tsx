f134a4dab3b7c8c8bd2b3243b35dfd1b055cafe9
Ryan <ryanshaunsmith@gmail.com>
2024-10-15 16:40:00 +0100

    formatting changes

--- src/App.tsx
+++ src/App.tsx
@@ -39,7 +39,12 @@ function App() {
         <label>
           What is on your todo list?:
           <br />
-          <input className="todo-input" type="text" value={todo} onChange={handleChange} />
+          <input
+            className="todo-input"
+            type="text"
+            value={todo}
+            onChange={handleChange}
+          />
         </label>
         <button className="todo-submit" type="submit">
           Add Todo
@@ -52,7 +57,24 @@ function App() {
               <li className="todo-item" key={td.id}>
                 {td.todo}
                 <button type="button" onClick={() => handleDelete(td.id)}>
-                <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" fill="#fff" version="1.1" id="Capa_1" width="1em" height="1em" viewBox="0 0 485 485" xmlSpace="preserve"><g><g><rect x="67.224" width="350.535" height="71.81"/><path d="M417.776,92.829H67.237V485h350.537V92.829H417.776z M165.402,431.447h-28.362V146.383h28.362V431.447z M256.689,431.447    h-28.363V146.383h28.363V431.447z M347.97,431.447h-28.361V146.383h28.361V431.447z"/></g></g></svg>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    xmlnsXlink="http://www.w3.org/1999/xlink"
+                    fill="#fff"
+                    version="1.1"
+                    id="Capa_1"
+                    width="1em"
+                    height="1em"
+                    viewBox="0 0 485 485"
+                    xmlSpace="preserve"
+                  >
+                    <g>
+                      <g>
+                        <rect x="67.224" width="350.535" height="71.81" />
+                        <path d="M417.776,92.829H67.237V485h350.537V92.829H417.776z M165.402,431.447h-28.362V146.383h28.362V431.447z M256.689,431.447    h-28.363V146.383h28.363V431.447z M347.97,431.447h-28.361V146.383h28.361V431.447z" />
+                      </g>
+                    </g>
+                  </svg>
                 </button>
               </li>
             );
