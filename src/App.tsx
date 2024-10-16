61a5af6db64dab3753ebfc30151356d0c7b9d9a9
Ryan <ryanshaunsmith@gmail.com>
2024-10-16 08:28:08 +0100

    add edit todo button

--- src/App.tsx
+++ src/App.tsx
@@ -38,6 +38,12 @@ function App() {
     ]);
   };
 
+  // To handle a todo item being clicked
+  // select the todo item being updated
+  // create an input field to update the todo item
+  // update the todo item with the new value
+  // remove the input field and display the updated todo item
+
   return (
     <div className="wrapper">
       <h1>What ToDo</h1>
@@ -63,6 +69,49 @@ function App() {
             return (
               <li className="todo-item" key={td.id}>
                 {td.todo}
+                <button type="button">
+                  <svg
+                    width="1rem"
+                    height="1rem"
+                    viewBox="0 0 21.00 21.00"
+                    version="1.1"
+                    xmlns="http://www.w3.org/2000/svg"
+                    xmlnsXlink="http://www.w3.org/1999/xlink"
+                    fill="#000"
+                  >
+                    <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                    <g
+                      id="SVGRepo_tracerCarrier"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    ></g>
+                    <g id="SVGRepo_iconCarrier">
+                      <g
+                        id="Page-1"
+                        stroke-width="0.00021000000000000004"
+                        fill="none"
+                        fill-rule="evenodd"
+                      >
+                        {' '}
+                        <g
+                          id="Dribbble-Light-Preview"
+                          transform="translate(-99.000000, -400.000000)"
+                          fill="#fff"
+                        >
+                          <g
+                            id="icons"
+                            transform="translate(56.000000, 160.000000)"
+                          >
+                            <path
+                              d="M61.9,258.010643 L45.1,258.010643 L45.1,242.095788 L53.5,242.095788 L53.5,240.106431 L43,240.106431 L43,260 L64,260 L64,250.053215 L61.9,250.053215 L61.9,258.010643 Z M49.3,249.949769 L59.63095,240 L64,244.114985 L53.3341,254.031929 L49.3,254.031929 L49.3,249.949769 Z"
+                              id="edit-[#1479]"
+                            ></path>
+                          </g>
+                        </g>
+                      </g>
+                    </g>
+                  </svg>
+                </button>
                 <button type="button" onClick={() => handleDelete(td.id)}>
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
